d398ed84c465473bc85b05fef7556e899b1b2854
bengtfrost <bengtfrost@gmail.com>
2026-05-30 19:23:35 +0200

    fix: treesitter v1 highlight + add rust, cpp parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,39 @@
 -- lua/plugins/treesitter.lua
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile" },
   config = function()
-    require('nvim-treesitter').setup({
+    require("nvim-treesitter").setup({
       ensure_installed = {
-        'python', 'javascript', 'typescript', 'lua', 'query',
-        'vim', 'vimdoc', 'c', 'bash', 'html', 'css', 'json',
-        'yaml', 'markdown', 'markdown_inline',
+        "python",
+        "javascript",
+        "typescript",
+        "lua",
+        "query",
+        "vim",
+        "vimdoc",
+        "rust",
+        "c",
+        "cpp",
+        "bash",
+        "html",
+        "css",
+        "json",
+        "yaml",
+        "markdown",
+        "markdown_inline",
       },
-      -- highlight and indent are top-level in v1 (no configs sub-module)
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      },
+    })
+
+    -- v1: highlight and indent are enabled via vim.treesitter directly
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(args)
+        local ok = pcall(vim.treesitter.start, args.buf)
+        if not ok then
+          vim.bo[args.buf].syntax = "on"
+        end
+      end,
     })
   end,
 }
