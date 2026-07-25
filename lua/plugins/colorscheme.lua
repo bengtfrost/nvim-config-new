0605f971d4ef36a02919b03eaef5e33349954d3f
bengtfrost <bengtfrost@gmail.com>
2026-07-25 09:17:53 +0200

    colorscheme update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,25 +1,19 @@
 -- lua/plugins/colorscheme.lua
 return {
-  {
-    "marko-cerovac/material.nvim",
-    priority = 1000,
-    lazy = false,
-    config = function()
-      require("material").setup({
-        contrast = {
-          terminal = true,       -- Set to true if you prefer native background/opacity
-          sidebars = true,       -- Enable contrast for sidebars
-          floating_windows = true, -- Enable contrast for floating windows
-          cursor_line = true,    -- Enable contrast for the cursor line
-          non_current_windows = true, -- Enable contrast for non-current windows
-        },
-        styles = {
-          comments = { italic = true },
-          keywords = { bold = true },
-        },
-      })
-      vim.g.material_style = "darker"
-      vim.cmd.colorscheme("material")
-    end,
-  },
+	{
+		"EdenEast/nightfox.nvim",
+		priority = 1000,
+		lazy = false,
+		config = function()
+			require("nightfox").setup({
+				options = {
+					styles = {
+						comments = "italic",
+						keywords = "bold",
+					},
+				},
+			})
+			vim.cmd.colorscheme("carbonfox")
+		end,
+	},
 }
